703ebcb247127be1bdea321c85c922d1efebf500
Brandon Slade <brandonyoyoslade@gmail.com>
2025-05-17 18:27:59 -0600

    refactor: migrate draggable number to Lit

--- src/components/draggable-number/index.ts
+++ src/components/draggable-number/index.ts
@@ -1,59 +1,58 @@
+import { LitElement, html, css } from 'lit';
 import { process_drag } from '../../wasm-bindings/cc_web_components.js';
 
-export class DraggableNumber extends HTMLElement {
-    static get observedAttributes() {
-        return ['value'];
-    }
+export class DraggableNumber extends LitElement {
+    static styles = css`
+        :host {
+            display: inline-block;
+        }
+    `;
+
+    static properties = {
+        value: { type: Number, reflect: true }
+    };
 
     private _dragging = false;
     private _startValue = 0;
     private _startX = 0;
 
-    constructor() {
-        super();
-        const shadow = this.attachShadow({ mode: 'open' });
-        shadow.innerHTML = `<input type="number" />`;
+    value = 0;
+
+    render() {
+        return html`<input
+            type="number"
+            .value=${String(this.value)}
+            @change=${this._onChange}
+            @pointerdown=${this._onPointerDown}
+            @pointermove=${this._onPointerMove}
+            @pointerup=${this._stopDrag}
+            @pointercancel=${this._stopDrag}
+        />`;
     }
 
-    connectedCallback() {
-        const input = this.shadowRoot?.querySelector('input') as HTMLInputElement | null;
-        if (!input) return;
-
-        input.addEventListener('change', () => {
-            this.setAttribute('value', input.value);
-            this.dispatchEvent(new Event('change'));
-        });
-
-        input.addEventListener('pointerdown', (e: PointerEvent) => {
-            this._dragging = true;
-            this._startX = e.clientX;
-            this._startValue = parseFloat(this.getAttribute('value') ?? '0');
-            input.setPointerCapture(e.pointerId);
-        });
-
-        input.addEventListener('pointermove', (e: PointerEvent) => {
-            if (!this._dragging) return;
-            const delta = e.clientX - this._startX;
-            const newVal = this._startValue + process_drag(delta);
-            this.setAttribute('value', String(newVal));
-            this.dispatchEvent(new Event('change'));
-        });
-
-        const stopDrag = () => {
-            this._dragging = false;
-        };
-
-        input.addEventListener('pointerup', stopDrag);
-        input.addEventListener('pointercancel', stopDrag);
+    private _onChange(e: Event) {
+        const input = e.target as HTMLInputElement;
+        this.value = parseFloat(input.value);
+        this.dispatchEvent(new Event('change'));
     }
 
-    attributeChangedCallback(name: string, _oldVal: string | null, newVal: string | null) {
-        if (name === 'value' && this.shadowRoot) {
-            const input = this.shadowRoot.querySelector('input');
-            if (input) {
-                (input as HTMLInputElement).value = newVal ?? '';
-            }
-        }
+    private _onPointerDown(e: PointerEvent) {
+        const input = e.target as HTMLInputElement;
+        this._dragging = true;
+        this._startX = e.clientX;
+        this._startValue = this.value;
+        input.setPointerCapture(e.pointerId);
+    }
+
+    private _onPointerMove(e: PointerEvent) {
+        if (!this._dragging) return;
+        const delta = e.clientX - this._startX;
+        this.value = this._startValue + process_drag(delta);
+        this.dispatchEvent(new Event('change'));
+    }
+
+    private _stopDrag() {
+        this._dragging = false;
     }
 }
 
